9e881a3b9ed4a9256a5d0a1e34a63230f02323e5
heshamahmed <ec22518@qmul.ac.uk>
2025-09-04 22:32:18 +0300

    feat: added apple sign in as an alternative login method using expo-apple-authentication

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,8 @@
+import * as AuthSession from 'expo-auth-session';
 export const authConfig = {
-  clientId: '15c0a23f-2d95-4678-b0fa-42603b3e2c45',
-  redirectUri: 'https://auth.expo.io/%40hesham12366/hertssu',
+  clientId: process.env.EXPO_PUBLIC_MS_CLIENT_ID,
+  redirectUri: AuthSession.makeRedirectUri({
+    scheme: 'hertssu',
+    path: 'redirect',
+  })
 };
